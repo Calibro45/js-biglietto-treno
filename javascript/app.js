72dd851f58de46acd7b5b73a2d62f9373cfac1f8
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-04 17:03:33 +0100

    somma totale decimale

--- javascript/app.js
+++ javascript/app.js
@@ -10,18 +10,18 @@
 
 // chiedo i km da fare
 
-const KM = parseInt( prompt("Quanti km devi percorrere?") );
+const KM = parseInt(prompt("Quanti km devi percorrere?"));
 console.log(KM);
 
 // chiedo l'età
 
-const userAge = parseInt ( prompt ("quanti anni hai?") );
+const userAge = parseInt(prompt("quanti anni hai?"));
 console.log(userAge);
 
 // prezzo biglietto
 //  - prezzo 0.21€ al km
 
-const TICKETPRICE = Number("0.21");
+const TICKETPRICE = 0.21;
 console.log(TICKETPRICE);
 
 // calcolo prezzo biglietto
@@ -32,12 +32,12 @@ console.log(TICKETTOT);
 
 // prezzo sconto 20%
 
-const firstDiscount = ( TICKETTOT * 20 ) / 100;
+const firstDiscount = (TICKETTOT*20)/100;
 console.log(firstDiscount);
 
 // prezzo sconto 40%
 
-const secondDiscount = ( TICKETTOT * 40 ) / 100;
+const secondDiscount = (TICKETTOT*40)/100;
 console.log(secondDiscount);
 
 //  -SE minorenne <=17 anni
@@ -48,11 +48,11 @@ console.log(secondDiscount);
 //      prezzo intero
 
 if (userAge <= 17) {
-    console.log(firstDiscount);
+    console.log((TICKETTOT-firstDiscount).toFixed(2));
 
 } else if (userAge >= 65) {
-    console.log(secondDiscount);
-    
+    console.log((TICKETTOT-secondDiscount).toFixed(2));
+
 } else {
     console.log(TICKETTOT);
 }
